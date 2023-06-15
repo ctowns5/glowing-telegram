e51f95cac1789cbe3f0d416d015e8a4ecba08b99
ctowns5 <128754073+ctowns5@users.noreply.github.com>
2023-06-15 00:03:29 -0600

    got npm packages installed, js written, tests seem to work. will polish it more next time

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Square, Triangle } = require("./lib/shapes");
 
+function makeFile(fileName, answers) {
+    let svgParts = "";
+    svgParts =
+        '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svgParts += `${answers.shape}`;
+    let shapeChoice;
+    if (answers.shape === "Triangle") {
+        shapeChoice = new Triangle();
+        svgParts += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+    } else if (answers.shape === "Square") {
+        shapeChoice = new Square();
+        svgParts += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+    } else {
+        shapeChoice = new Circle();
+        svgParts += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+    }
+    svgParts += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    svgParts += "</svg>";
+
+    fs.writeFile(fileName, svgParts, (err) => {
+        err ? console.log(err) : console.log("Generated logo.svg");
+    });
+    }
+
 function inquire() {
     inquirer.prompt([
         {
